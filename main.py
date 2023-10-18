1469f0dd745509ae56160df413fa38e4a681b094
Dillon Harris <43145893+dillonkh@users.noreply.github.com>
2023-10-18 16:58:15 -0500

    refactor: rename to app

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
-from walmart_alert_api.api.alert import router as alerts_router
+from app.api.v1.alert import router as v1_router
+from app.api.v2.alert import router as v2_router
+from app.models.custom_exception import AlertException
 
 app = FastAPI()
-app.include_router(alerts_router)
+app.include_router(v1_router)
+app.include_router(v2_router)
+
+
+@app.exception_handler(AlertException)
+async def alert_exception_handler(request: Request, e: AlertException):
+    return JSONResponse(
+        status_code=e.status_code,
+        content={
+            "success": False,
+            "alert_id": e.alert_id,
+            "service_id": e.service_id,
+            "error": e.error_message,
+        },
+    )
 
 
 @app.get("/")
